3cd7c378d8daadca5b131e4421703eddff397a23
Pranav Chaudhari <pranavchaudhari499@gmail.com>
2026-02-02 20:20:01 +0530

    auth middleware

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const healthRoute = require("./routes/health.route");
+const authRoute = require("./routes/auth.route");
+const protectedRoute = require("./routes/protected.route");
+
+
+app.use("/health", healthRoute);
+app.use("/auth", authRoute);
+app.use("/protected", protectedRoute);
+
+module.exports = app;
